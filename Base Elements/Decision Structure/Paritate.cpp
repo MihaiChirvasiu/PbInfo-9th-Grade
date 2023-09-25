c9e1468740bb4858f09ac0ec128c704516350046
Mihai Chirvasiu <100131781+MihaiChirvasiu@users.noreply.github.com>
2023-09-25 12:10:33 +0300

    Create Paritate.cpp

---
+++ Base Elements/Decision Structure/Paritate.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int n;
+int main()
+{    cin>> n ;
+      if (n % 2==0 ) cout << n << " este par" ;
+      if (n % 2==1 ) cout << n << " este impar" ;
+
+    return 0;
+}
