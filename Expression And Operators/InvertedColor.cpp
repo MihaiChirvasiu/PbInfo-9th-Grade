2d646739c60e8549e01bb5d2070e68ec45e83fde
Mihai Chirvasiu <100131781+MihaiChirvasiu@users.noreply.github.com>
2023-09-25 11:44:25 +0300

    Create InvertedColor.cpp

---
+++ Expression And Operators/InvertedColor.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int r, g, b, R, G, B ;
+int main()
+{
+ cin >> r >> g >> b ;
+ R = 255 - r ;
+ G = 255 - g ;
+ B = 255 - b ;
+ cout << R << ' ' << G << ' ' << B ;
+
+  return 0 ;
+}
