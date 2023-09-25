88e0d9bfbe11ac1871bd58fcab1f8c6a2b7eff8a
Mihai Chirvasiu <100131781+MihaiChirvasiu@users.noreply.github.com>
2023-09-25 12:41:27 +0300

    Create Marte1.cpp

---
+++ Base Elements/Expressions And Operators/Marte1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int a, b, c, n, m, p ;
+int main()
+{
+  cin >> a >> b >> c ;
+  cin >> n >> m >> p ;
+  cout << a * n + b * m + c * p ;
+    return 0;
+}
