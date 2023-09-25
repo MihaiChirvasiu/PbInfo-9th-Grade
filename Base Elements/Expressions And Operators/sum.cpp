632cd342380e4ddcb8cd76855c6eca0a9720fca3
Mihai Chirvasiu <100131781+MihaiChirvasiu@users.noreply.github.com>
2023-09-25 12:57:16 +0300

    Create sum.cpp

---
+++ Base Elements/Expressions And Operators/sum.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int a,b,s=0;
+int main()
+{
+   ifstream f("sum.in");
+   ofstream g("sum.out");
+   f>>a>>b;
+   s=a+b;
+   g<<s;
+    return 0;
+}
