3d724f0697cc21d1a5a9bd10eeb6d594c1cbe84a
Mihai Chirvasiu <100131781+MihaiChirvasiu@users.noreply.github.com>
2023-09-27 14:23:13 +0300

    Create Concurs1.cpp

---
+++ Base Elements/Decision Structure/Concurs1.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int a, n, b ;
+int main()
+{
+  cin >> a >> b >> n ;
+  if ( a <= n && n <= b )
+    cout << "DA" ;
+  else
+    cout << "NU" ;
+    return 0;
+}
